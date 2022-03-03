89b40ac40452be0248e3daa09c0352d90d208aea
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2022-03-03 22:19:24 +0100

    Create ssh_auto_setup.py

---
+++ ssh_auto_setup.py
@@ -0,0 +1,35 @@
+import paramiko
+import sys
+import concurrent.futures
+import asyncio 
+
+try:
+    host = sys.argv[1]
+    port = sys.argv[2]
+    username = sys.argv[3]
+    password = sys.argv[4]
+except:
+    print('Usage:\npython3 cfiptables.py host port username pass\n\nExample usage:\npython3 cfiptables 123.123.123.123 22 root root')
+
+def Server(host=host,port=port,username=username,password=password,*,cmd):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, port, username, password)
+    except:
+        sys.exit('Invalid host port username password')
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    return stderr
+
+
+def Main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+        executor.submit(Server(cmd='sudo rm -r cloudflare.sh'))
+        executor.submit(Server(cmd='wget https://gist.githubusercontent.com/Manouchehri/cdd4e56db6596e7c3c5a/raw/be3c3ef3459a39e97aad4e643c9e0992d49cef96/cloudflare.sh'))
+        executor.submit(Server(cmd='sudo apt update -y'))
+        executor.submit(Server(cmd='sudo apt install curl -y'))
+        executor.submit(Server(cmd='chmod +x cloudflare.sh && sudo ./cloudflare.sh'))
+    print("\033[32mInstalled Successfully Iptables Ruleset")
+
+if __name__ == '__main__':
+    Main()
