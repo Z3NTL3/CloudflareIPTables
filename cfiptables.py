e0f7bb2a26ce78648f5b21212ec247af4730ebb8
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2022-04-08 15:11:12 +0200

    Update cfiptables.py

--- cfiptables.py
+++ cfiptables.py
@@ -1,6 +1,8 @@
 import paramiko
 import sys
 from concurrent.futures import ThreadPoolExecutor
+import asyncio
+
 '''
 By Z3NTL3
 '''
@@ -9,7 +11,7 @@ commandos  = [
     'sudo rm -r anti_ddos_cf_iptables.py',
     'wget https://raw.githubusercontent.com/Z3NTL3/CloudflareIPTables/main/anti_ddos_cf_iptables.py',
     'sudo apt update -y',
-    'sudo apt install python3',
+    'sudo apt install python3 -y',
     'sudo apt install curl -y',
     'chmod +x anti_ddos_cf_iptables.py && sudo python3 anti_ddos_cf_iptables.py'
 ]
@@ -20,7 +22,8 @@ try:
     username = sys.argv[3]
     password = sys.argv[4]
 except:
-    print('Usage:\npython3 cfiptables.py host port username pass\n\nExample usage:\npython3 cfiptables 123.123.123.123 22 root root')
+    print(f'Usage:\npython3 {__file__} host port username pass\n\nExample usage:\npython3 {__file__} 123.123.123.123 22 root root')
+    sys.exit(0)
 
 
 def Server(host=host,port=port,username=username,password=password,*,cmd):
@@ -34,14 +37,19 @@ def Server(host=host,port=port,username=username,password=password,*,cmd):
     except:
         sys.exit('\033[31mERROR: \033[35mInvalid host port username password\033[0m')
     stdin, stdout, stderr = ssh.exec_command(cmd)
-    return stdout
+    return stdin
 
+async def Run():
+    await asyncio.gather(Main())
 
-def Main():
+async def Main():
     with ThreadPoolExecutor(max_workers=6) as executor:
         for cmd in commandos:
             executor.submit(Server(cmd=f'{cmd}'))
+            await asyncio.sleep(0)
+
     print("\033[32m\033[1mInstalled Successfully Iptables Ruleset\033[0m")
 
 if __name__ == '__main__':
-    Main()
+    lp = asyncio.new_event_loop()
+    lp.run_until_complete(Run())
