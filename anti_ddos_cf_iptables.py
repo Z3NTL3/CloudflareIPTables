7f7c2188d3eb5edabb634f2067787009a5a3c7c9
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2023-04-17 11:00:31 +0200

    Update anti_ddos_cf_iptables.py

--- anti_ddos_cf_iptables.py
+++ anti_ddos_cf_iptables.py
@@ -1,57 +1,37 @@
-import logging
-import subprocess as sp
-import sys
-'''
-Programmed by Z3NTL3
-'''
+import httpx
+import subprocess
 
-logging.basicConfig(filename='logs.log', level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+try:
+    # clear first
+    p = subprocess.Popen("ufw reset", shell=True, stdin=subprocess.PIPE)
+    p.communicate(input='y\n'.encode())
 
-def main():
-    
-    ipv6 = "curl -s https://www.cloudflare.com/ips-v6"
-    ipv4 = "curl -s https://www.cloudflare.com/ips-v4"
+    p = subprocess.Popen("ufw default deny incoming",
+                         shell=True, stdin=subprocess.PIPE)
+    p.communicate(input='y\n'.encode())
 
-    contentipv4 = sp.getoutput(ipv4).strip()
-    contentipv6 = sp.getoutput(ipv6).strip()
+    with httpx.Client(headers={"Cache-Control": "must-revalidate", "Content-Type": "text/plain"}) as client:
+        ipv4s = client.get("https://www.cloudflare.com/ips-v4").text
+        ipv6s = client.get("https://www.cloudflare.com/ips-v6").text
 
+        ipv4s = ipv4s.strip().split("\n")
+        ipv6s = ipv6s.strip().split("\n")
 
-    sp.run("sudo rm -r ipv4.txt", shell=True, stderr=sp.DEVNULL)
-    sp.run("sudo rm -r ipv6.txt", shell=True, stderr=sp.DEVNULL)
+        for ipv4 in ipv4s:
+            p = subprocess.Popen(
+                f"ufw allow proto tcp from {ipv4} comment 'CF IP'", shell=True, stdin=subprocess.PIPE)
+            p.communicate(input='y\n'.encode())
+        for ipv6 in ipv6s:
+            p = subprocess.Popen(
+                f"ufw allow proto tcp from {ipv6} comment 'CF IP'", shell=True, stdin=subprocess.PIPE)
+            p.communicate(input='y\n'.encode())
 
-    readableipv4 = contentipv4.split("\n")
-
-    readableipv6 = contentipv6.split("\n")
-
- 
-
-    try:
-        for ipv4s in readableipv4:
-            sp.run("iptables -I INPUT -p tcp -m multiport --dports http,https -s {} -j ACCEPT".format(ipv4s),shell=True)
-    except:   
-        pass
-    
-    try:
-
-        for ipv6s in readableipv6:
-            sp.run("ip6tables -I INPUT -p tcp -m multiport --dports http,https -s {} -j ACCEPT".format(ipv6s),shell=True)
-    
-    except:
-        pass
-    
-
-
-    try:
-        sp.run("iptables -A INPUT -p tcp --dport http,https -j DROP",shell=True)
-        sp.run("ip6tables -A INPUT -p tcp --dport http,https -j DROP",shell=True)
-    except:
-        pass
-    
-    logging.info("Successfully set IPTABLE RULES")
-    print("Successfully allowed ONLY CLOUDFLARE IPs REVERSE PROXY TECHNOLOGY and disallowed all requests from other.\nAllowed only IP requests which came from the cloudflare cdn reverse proxy end")
-
-    
-
-
-if __name__ == '__main__':
-    main()
+        p = subprocess.Popen("ufw allow ssh",
+                             shell=True)
+        p.communicate(input='y\n'.encode())
+        p = subprocess.Popen("ufw reload", shell=True, stdin=subprocess.PIPE)
+        p.communicate(input='y\n'.encode())
+        p = subprocess.Popen("ufw enable", shell=True, stdin=subprocess.PIPE)
+        p.communicate(input='y\n'.encode())
+except:
+    print("Failed")
